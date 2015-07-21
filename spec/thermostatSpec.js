06ff1b7b58a31e65c52c0552b702eeadfa34969f
Oliver <o.lucas@live.com>
2015-07-21 11:42:36 +0100

    written test for increasing temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,6 +11,16 @@ describe("by default", function(){
   it("thermostat should start at 20 degrees", function(){
     expect(thermostat.temperature).toEqual(20)
   });
+  
+});
+
+describe("changing temperature", function(){
+
+  it("should be able to increase temperature", function(){
+    thermostat.increaseTemp(1)
+    expect(thermostat.temperature).toEqual(21)
+  });
+
 });
 
 
