e4f65ffec6e757f7ceeca907222c20457366503e
Oliver <o.lucas@live.com>
2015-07-21 11:45:55 +0100

    test written for decreasing temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,7 +11,7 @@ describe("by default", function(){
   it("thermostat should start at 20 degrees", function(){
     expect(thermostat.temperature).toEqual(20)
   });
-  
+
 });
 
 describe("changing temperature", function(){
@@ -19,6 +19,11 @@ describe("changing temperature", function(){
   it("should be able to increase temperature", function(){
     thermostat.increaseTemp(1)
     expect(thermostat.temperature).toEqual(21)
+  });
+
+  it("should be able to decrease temperature", function(){
+    thermostat.decreaseTemp(1)
+    expect(thermostat.temperature).toEqual(19)
   });
 
 });
